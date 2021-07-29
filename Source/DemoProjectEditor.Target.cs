c3295b48f43b92e24d5d6e79b6b8cd711ebdde7e
Michael Neumann <neumann.michael@uni-bremen.de>
2021-07-29 15:34:49 +0200

    4.25

--- Source/DemoProjectEditor.Target.cs
+++ Source/DemoProjectEditor.Target.cs
@@ -8,6 +8,7 @@ public class DemoProjectEditorTarget : TargetRules
 	public DemoProjectEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
 
 		ExtraModuleNames.AddRange( new string[] { "DemoProject" } );
 	}
